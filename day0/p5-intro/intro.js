c60dc74b73f812c91c6ed6be0c58d1269045cfce
jefferyASC6 <jkeys21@newarka.edu>
2019-07-15 11:15:59 -0400

    second commit

---
+++ day0/p5-intro/intro.js
@@ -0,0 +1,12 @@
+// set up canvas
+function setup(){
+createCanvas(500,500);
+background(255,0,0);
+};
+
+// draw on canvas
+function draw(){
+//console.log("Nice to say Hello!")
+
+circle(100,100,80);
+};
